1694c52b6a30107ce7b39de47c2fd977ef1e5be2
Vencislav <vencislav67@hotmail.com>
2022-07-26 16:45:22 +0300

    Add project files.

---
+++ 01_ClassBoxData/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_ClassBoxData
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
