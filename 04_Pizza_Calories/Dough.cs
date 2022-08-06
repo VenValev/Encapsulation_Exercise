e352e2750bd719d0c01b56e6455c67eac55281cf
Vencislav <vencislav67@hotmail.com>
2022-08-06 12:39:53 +0300

    4

---
+++ 04_Pizza_Calories/Dough.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04_Pizza_Calories
+{
+    public class Dough
+    {
+        private const string errMsg = "Invalid type of dough.";
+        private string dType;
+        private string dBake;
+        private const double caloriesPerGram = 2;
+        private double dWeight;
+
+        public string DType
+        {
+            get { return dType; }
+            set
+            {
+                if(String.IsNullOrWhiteSpace(value))
+                {
+
+                }
+            }
+        }
+
+    }
+}
---
+++ 04_Pizza_Calories/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04_Pizza_Calories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
