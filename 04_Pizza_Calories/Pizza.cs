2969e33b5289cbc30a5fcf1fd2082648ef5c846a
Vencislav <vencislav67@hotmail.com>
2022-08-06 14:38:26 +0300

    4

---
+++ 04_Pizza_Calories/Pizza.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04_Pizza_Calories
+{
+    public class Pizza
+    {
+        private string name;
+        private List<Topping> toppingType;
+        private Dough doughType;
+        private int toppingCount;
+        private int totalCalories;
+
+        public Pizza()
+        {
+            toppingType = new List<Topping>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                this.name = value;
+            }
+        }
+    }
+}
