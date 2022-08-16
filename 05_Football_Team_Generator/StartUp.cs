8513de3f0fd349bb4da6fb2fe8df95743a6e994a
Vencislav <vencislav67@hotmail.com>
2022-08-16 12:28:45 +0300

    5

---
+++ 05_Football_Team_Generator/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05_Football_Team_Generator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
