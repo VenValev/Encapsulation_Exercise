ad103474950666df80c8a78d7de763246d177274
Vencislav <vencislav67@hotmail.com>
2022-08-16 12:58:53 +0300

    5

--- 05_Football_Team_Generator/Team.cs
+++ 05_Football_Team_Generator/Team.cs
@@ -7,6 +7,17 @@ namespace _05_Football_Team_Generator
     public class Team
     {
         private string name;
+        private int rating;
+        private readonly List<Player> players;
 
+        public Team()
+        {
+            players = new List<Player>();
+        }
+        public Team(string name)
+            :this()
+        {
+
+        }
     }
 }
